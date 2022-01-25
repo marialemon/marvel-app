ee7af80ac2960b284e4a60a31ec7612c441bc8b6
maria.nunez <maria.nunez@wefox.com>
2022-01-25 23:51:52 +0100

    change img

--- app/src/main/java/com/marianunez/marvelapp/domain/model/mapper/CharacterMapper.kt
+++ app/src/main/java/com/marianunez/marvelapp/domain/model/mapper/CharacterMapper.kt
@@ -3,5 +3,10 @@ package com.marianunez.marvelapp.domain.model.mapper
 import com.marianunez.marvelapp.data.network.response.CharacterResponse
 import com.marianunez.marvelapp.domain.model.CharacterModel
 
-fun CharacterResponse.toModel(): CharacterModel =
-    CharacterModel(id, name, description, imgUrl = "${thumbnail.path}.${thumbnail.extension}")
+
+fun CharacterResponse.toModel(): CharacterModel {
+    val imgUrl2 = "${thumbnail.path}.${thumbnail.extension}"
+    val imgUrl = "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+
+    return CharacterModel(id, name, description, imgUrl = imgUrl)
+}
