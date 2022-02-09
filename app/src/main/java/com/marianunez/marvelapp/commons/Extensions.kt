73d31e93023577590438a8273bb2bad719615148
maria.nunez <maria.nunez@wefox.com>
2022-02-09 14:18:03 +0100

    add placeholder color

--- app/src/main/java/com/marianunez/marvelapp/commons/Extensions.kt
+++ app/src/main/java/com/marianunez/marvelapp/commons/Extensions.kt
@@ -1,13 +1,16 @@
 package com.marianunez.marvelapp.commons
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
-fun ImageView.loadImageByUrl(url: String) {
+fun ImageView.loadImageByUrl(url: String?) {
 
     Glide.with(context)
         .load(url)
         .centerCrop()
+        .placeholder(ColorDrawable(Color.LTGRAY))
         .into(this)
 
 }
